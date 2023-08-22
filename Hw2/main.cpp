1e24ae95a650cc7c4690457047607ccd425dcb5f
YangWithU <plzw8@outlook.com>
2023-08-22 22:40:52 +0800

    nightly

--- Hw2/main.cpp
+++ Hw2/main.cpp
@@ -1,17 +1,21 @@
-
 #include <iostream>
 #include <gtest/gtest.h>
 #include "client.h"
 #include "server.h"
+
+#include <chrono>
+#include <random>
 
 
 int main(int argc, char **argv)
 {
     if (true) // make false to run unit-tests
     {
-        std::string s = "abc";
-
-        std::cout << s;
+        std::mt19937_64 seed(std::chrono::steady_clock::now().time_since_epoch().count());
+        std::uniform_int_distribution<int> gen_rand(1, 9);
+        for(int i = 0; i < 5; i++) {
+            std::cout << gen_rand(seed) << " ";
+        }
     }
     else
     {
--- Hw2/server.cpp
+++ Hw2/server.cpp
@@ -1,13 +1,39 @@
 #include "server.h"
 #include "client.h"
 
+#include <random>
+#include <chrono>
+
 // pending transaction
 std::vector<std::string> pending_trxs;
 
 Server::Server() = default;
 
 std::shared_ptr<Client> Server::add_client(std::string id) {
-
+    bool add_rand = false;
+    for(auto &x : clients) {
+        if(x.first->get_id() == id) {
+            add_rand = true;
+            break;
+        }
+    }
+    if(add_rand) {
+        std::mt19937_64 generator(std::chrono::steady_clock::now().time_since_epoch().count());
+        std::uniform_int_distribution<int> gen_dist(1, 9);
+        for(int i = 0; i < 4; i++) {
+            id += std::to_string(gen_dist(generator));
+        }
+    }
+    auto cli_ptr = std::make_shared<Client>(id, *this);
+    clients[cli_ptr] = 5.0;
+    return cli_ptr;
 }
 
+std::shared_ptr<Client> Server::get_client(std::string id) {
+    for(auto &x : clients) {
+        if(x.first->get_id() == id) {
+            return x.first;
+        }
+    }
+}
 
